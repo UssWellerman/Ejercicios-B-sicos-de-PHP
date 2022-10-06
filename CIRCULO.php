b369f2b7b2a028ab10e5781adb08d9199c72ffe5
UssWellerman <110182883+UssWellerman@users.noreply.github.com>
2022-10-06 11:18:29 -0500

    Add files via upload

---
+++ CIRCULO.php
@@ -0,0 +1,17 @@
+<?php
+   //Area de un triangulo//
+   $altura=15;
+   $base=12;
+   $a=$base*$altura/2;
+      echo "El area del triangulo es $a cm <br>";
+   //Area de un circulo//
+   $pi=3.1416;
+   $radio=19;
+   $b=$pi*$radio*$radio;
+      echo "El area del circulo es $b cm <br>";
+   //Area de un cuadrado//
+   $lado1=12;
+   $lado2=12;
+   $c=$lado1*$lado2;
+      echo "El area del cuadrado es $c cm";
+?>
